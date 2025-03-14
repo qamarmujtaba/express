ca5ab449ad930fdaf9ce70c207fce71a21fb52ee
qamarmujtaba <qamarmujtaba9913@gmail.com>
2025-03-14 12:11:33 +0500

    updated index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is your Express app running with PM2!");
+  res.send("Hello, this is your Express app running with PM2! deploying through pipeline");
 });
 
 app.listen(PORT, () => {

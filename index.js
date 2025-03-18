02dc86a550d48b7d71221f14c9bebac6c40cda17
qamarmujtaba <qamarmujtaba9913@gmail.com>
2025-03-18 14:39:13 +0500

    dockerized

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is your Express app running with PM2! deploying through pipeline");
+  res.send("Hello, this is your Express app running with PM2! deploying through pipeline and Docker");
 });
 
 app.listen(PORT, () => {
